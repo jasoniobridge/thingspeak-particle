86788881518db0050a3b55c7df86b416cd99913f
jasoniobridge <jason@iobridge.com>
2016-06-20 15:57:58 -0400

    use dtoa for all particle.io devices

--- firmware/ThingSpeak.h
+++ firmware/ThingSpeak.h
@@ -1480,7 +1480,7 @@ private:
 			return ERR_OUT_OF_RANGE;
 		}
 		// Given that the resolution of Spark is 1 / 2^12, or ~0.00024 volts, assume that 5 places right of decimal should be sufficient for most applications
-        #ifdef PARTICLE_PHOTONELECTRON
+        #ifdef SPARK
           //Photon doesn't have a dtostrf, but does have dtoa
           dtoa((double)value,5, valueString);
         #else
